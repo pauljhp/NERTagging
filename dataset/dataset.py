b0fe85d84dbe7de80381b27af26e3e4c8353d513
pauljhp <paulpbmo@gmail.com>
2022-11-09 20:15:38 +0800

    initial commit

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -3,14 +3,21 @@ from torch.utils.data import DataLoader, Dataset
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from typing import Iterable, Sequence, Optional, Union, Any, Callable
+from typing import Iterable, Sequence, Optional, Union, Any, Callable, Tuple
+import re
+from transformers import BERTTokenizer
+import spacy
+
+
+BERT_TOKENIZER = BERTTokenizer.from_pretrained()
 
 
 class NERDataset(Dataset):
-    def load_data(data_path: Path):
+    def load_data(self, data_path: Path):
         with data_path.open("r") as f:
             txt = f.read()
-        headers = txt.split("\n\n")[0].split(" ")
+        headers = [re.sub("[\-]", "", h) for h in 
+                txt.split("\n\n")[0].split(" ")]
         sentences = [[tok.split(" ") for tok in sent.split("\n")] 
             for sent in txt.split("\n\n")[1:]
             ]
@@ -22,11 +29,26 @@ class NERDataset(Dataset):
             data=sent, 
             dtype='object', 
             index=headers).to_frame().T 
-            for sent in sentences])
+            for sent in sentences if sent.get("DOCSTART")])
         sentences.index = range(sentences.shape[0])
         return sentences
 
-    def __init__(self, data_path: str="./data/conll2003", mode: str='train'):
+    def __init__(self, 
+        data_path: str="./data/conll2003", 
+        mode: str='train', 
+        target_col: str='O'):
+        """
+        :param target_col: Takes 'X' for CFG parsing tasks or 'O' for NER tasks
+        """
         super(NERDataset, self).__init__()
         data_path = Path(data_path).joinpath(f"{mode}.txt")
-        self.data = self.load_data(data_path)
+        data = self.load_data(data_path)
+        self.data = data.loc[:, ["DOCSTART", target_col]]
+
+    def __getitem__(self, idx: int) -> Tuple[str, str]:
+        return tuple(self.data.loc[idx].values)
+
+    def __len__(self):
+        return self.data.shape[0]
+
+    def _get_vocabulary(cased: bool, tokenizer: str='spacy')
---
+++ models/transformerTagger.py
@@ -0,0 +1,97 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import math
+import numpy as np
+from typing import (Sequence, Iterable, Dict, Tuple)
+
+from dataset.dataset import NERDataset
+from torch.utils.data import DataLoader
+
+import logging
+
+logging.basicConfig(filename="./log/exceptions.log")
+logger = logging.getLogger()
+
+
+class TranformerTagger(nn.modules):
+    def __init__(self, hidden_layers, )
+
+
+# classes not included directly in torch
+
+class PositionalEncoding(nn.Module):
+    """Positional encoder"""
+    def __init__(self, d_model, 
+        dropout=0.1, 
+        max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
+
+def scaled_dot_product_attention(q, k, v, mask=None) -> Tuple[torch.tensor, torch.tensor]:
+    d_k = q.size()[-1]
+    attn_logits = torch.matmul(q, k.transpose(-2, -1))
+    attn_logits = attn_logits / math.sqrt(d_k)
+    if mask is not None:
+        attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+    attention = F.softmax(attn_logits, dim=-1)
+    values = torch.matmul(attention, v)
+    return values, attention
+
+class MultiheadAttention(nn.Module):
+    def __init__(self, input_dim, embed_dim, num_heads):
+        super(MultiheadAttention, self).__init__()
+        assert embed_dim % num_heads == 0, "Embed_dim must be multiples of num_heads!"
+
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+
+        # Stack all weight matrices 1...h together for efficiency
+        # Note that in many implementations you see "bias=False" which is optional
+        self.qkv_proj = nn.Linear(input_dim, 3 * embed_dim)
+        self.o_proj = nn.Linear(embed_dim, embed_dim)
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        # Original Transformer initialization, see PyTorch documentation
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.o_proj.weight)
+        self.o_proj.bias.data.fill_(0)
+
+    def forward(self, x, mask=None, return_attention=False):
+        batch_size, seq_length, embed_dim = x.size()
+        qkv = self.qkv_proj(x)
+
+        # Separate Q, K, V from linear output
+        qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3 * self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3)  # [Batch, Head, SeqLen, Dims]
+        q, k, v = qkv.chunk(3, dim=-1)
+
+        # Determine value outputs
+        values, attention = scaled_dot_product_attention(q, k, v, mask=mask)
+        values = values.permute(0, 2, 1, 3)  # [Batch, SeqLen, Head, Dims]
+        values = values.reshape(batch_size, seq_length, embed_dim)
+        o = self.o_proj(values)
+
+        if return_attention:
+            return o, attention
+        else:
+            return o
