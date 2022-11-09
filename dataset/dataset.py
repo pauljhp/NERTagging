ab0c39b3d15bcae93ce86268a26f7f2191a6cde1
pauljhp <paulpbmo@gmail.com>
2022-11-10 00:01:11 +0800

    initial commit

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -3,13 +3,17 @@ from torch.utils.data import DataLoader, Dataset
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from typing import Iterable, Sequence, Optional, Union, Any, Callable, Tuple
+from typing import Iterable, Sequence, Optional, Union, Any, Callable, Tuple, Set
 import re
-from transformers import BERTTokenizer
+from transformers import BertTokenizer
 import spacy
+from spacy.lang.en import English
+from spacy.tokenizer import Tokenizer
+import itertools
 
 
-BERT_TOKENIZER = BERTTokenizer.from_pretrained()
+SPACY_ENG = English()
+SPACY_TOKENIZER = Tokenizer(SPACY_ENG.vocab)
 
 
 class NERDataset(Dataset):
@@ -36,14 +40,20 @@ class NERDataset(Dataset):
     def __init__(self, 
         data_path: str="./data/conll2003", 
         mode: str='train', 
-        target_col: str='O'):
+        target_col: str='O',
+        cased: bool=False,
+        tokenizer: Optional[str]=None
+        ):
         """
         :param target_col: Takes 'X' for CFG parsing tasks or 'O' for NER tasks
+        :param tokenizer: Optional, takes 'spacy', 'bert'
         """
         super(NERDataset, self).__init__()
         data_path = Path(data_path).joinpath(f"{mode}.txt")
         data = self.load_data(data_path)
         self.data = data.loc[:, ["DOCSTART", target_col]]
+        self.tokens = set(itertools.chain(*self.data.DOCSTART.values))
+        self._vocab = self._get_vocabulary(cased, tokenizer)
 
     def __getitem__(self, idx: int) -> Tuple[str, str]:
         return tuple(self.data.loc[idx].values)
@@ -51,4 +61,28 @@ class NERDataset(Dataset):
     def __len__(self):
         return self.data.shape[0]
 
-    def _get_vocabulary(cased: bool, tokenizer: str='spacy')
+    def _get_vocabulary(self, 
+        cased: bool, 
+        tokenizer: Optional[str]=None) -> Set[str]:
+        """get the vocabulary occuring in the dataset
+        :param tokenizer: Optional, takes 'spacy', 'bert'"""
+        if tokenizer is None:
+            if cased:
+                return self.tokens
+            else:
+                return {t.lower() for t in self.tokens}
+        elif tokenizer in ['spacy']:
+            tokens = {SPACY_TOKENIZER(t) for t in self.tokens}
+            return {t.doc.text for t in tokens}
+        elif tokenizer in ["bert"]:
+            if cased:
+                bert_tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+            else:
+                bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+            return bert_tokenizer(list(self.tokens))
+        else:
+            raise NotImplementedError(f"unrecognized tokenizer {tokenizer}")
+    
+    @property
+    def vocab(self):
+        return self._vocab
--- models/transformerTagger.py
+++ models/transformerTagger.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 import math
 import numpy as np
-from typing import (Sequence, Iterable, Dict, Tuple)
+from typing import (Sequence, Iterable, Dict, Tuple, Callable)
 
 from dataset.dataset import NERDataset
 from torch.utils.data import DataLoader
@@ -13,30 +13,48 @@ from torch.utils.data import DataLoader
 import logging
 
 logging.basicConfig(filename="./log/exceptions.log")
-logger = logging.getLogger()
-
+LOGGER = logging.getLogger()
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class TranformerTagger(nn.modules):
-    def __init__(self, hidden_layers, )
+    def __init__(self, 
+        d_model: int=512,
+        n_head: int=8,
+        vocab_size: int=23000,
+        num_encoder_layers: int=8,
+        num_decoder_layers: int=8,
+        dim_feedforward: int=1024,
+        dropout: float=.1,
+        embedding_type: str="torch",
+        no_dense_layers: int = 5,
+        activation: Callable=F.relu,
+        batch_first: bool=True,
+        device: str=DEVICE,
+        ):
+        self.d_model = d_model
+        self.positional_encoder = PositionalEncoder(d_model, dropout)
+        self.embedding = get_embedder(embedding_type, 
+            um_embeddings=vocab_size, 
+            embedding_dim=d_model)
 
 
 # classes not included directly in torch
 
-class PositionalEncoding(nn.Module):
+class PositionalEncoder(nn.Module):
     """Positional encoder"""
-    def __init__(self, d_model, 
-        dropout=0.1, 
-        max_len=5000):
-        super(PositionalEncoding, self).__init__()
+    def __init__(self, 
+        d_model: int, 
+        dropout: float=0.1, 
+        max_len: int=10000):
+        super(PositionalEncoder, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
+        pe = torch.zeros(max_len, d_model) 
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
+        self.register_buffer('positional_encoder', pe)
 
     def forward(self, x):
         x = x + self.pe[:x.size(0), :]
@@ -44,15 +62,6 @@ class PositionalEncoding(nn.Module):
 
 
 
-def scaled_dot_product_attention(q, k, v, mask=None) -> Tuple[torch.tensor, torch.tensor]:
-    d_k = q.size()[-1]
-    attn_logits = torch.matmul(q, k.transpose(-2, -1))
-    attn_logits = attn_logits / math.sqrt(d_k)
-    if mask is not None:
-        attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
-    attention = F.softmax(attn_logits, dim=-1)
-    values = torch.matmul(attention, v)
-    return values, attention
 
 class MultiheadAttention(nn.Module):
     def __init__(self, input_dim, embed_dim, num_heads):
@@ -95,3 +104,25 @@ class MultiheadAttention(nn.Module):
             return o, attention
         else:
             return o
+
+
+# custom functions
+
+def scaled_dot_product_attention(q, k, v, mask=None) -> Tuple[torch.tensor, torch.tensor]:
+    d_k = q.size()[-1]
+    attn_logits = torch.matmul(q, k.transpose(-2, -1))
+    attn_logits = attn_logits / math.sqrt(d_k)
+    if mask is not None:
+        attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+    attention = F.softmax(attn_logits, dim=-1)
+    values = torch.matmul(attention, v)
+    return values, attention
+
+def get_embedder(embedding: str="torch", **kwargs):
+    if embedding in ["torch", "pytorch"]:
+        embedder = nn.Embedding(**kwargs)
+    elif embedding in ["glove"]:
+        raise NotImplementedError
+    else:
+        raise NotImplementedError
+    return embedder
